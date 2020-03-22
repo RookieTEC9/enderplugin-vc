713f27792195c7982ece79040b28207554dac9ec
RookieTEC9 <jeremiur.ps101q@gmail.com>
2020-03-22 17:36:08 -0400

    Pre 0.0.3

---
+++ io/github/rookietec9/Enderplugin/EnderPluginCommandExecutor.java
@@ -0,0 +1,25 @@
+package io.github.rookietec9.EnderPlugin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class EnderPluginCommandExecutor implements CommandExecutor {
+  private final EnderPlugin plugin;
+  
+  public EnderPluginCommandExecutor(EnderPlugin plugin) {
+    this.plugin = plugin;
+  }
+  
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    if (cmd.getName().equalsIgnoreCase("EnderSchedule"));
+    sender.sendMessage(ChatColor.DARK_PURPLE + "Server schedule as follows:");
+    sender.sendMessage(ChatColor.BLUE + ChatColor.BOLD + "REGULAR DAYS");
+    sender.sendMessage(ChatColor.BLUE + "WEEKDAYS:" + ChatColor.AQUA + "3PM-9PM");
+    sender.sendMessage(ChatColor.BLUE + "WEEKENDS:" + ChatColor.AQUA + "10AM-9PM");
+    sender.sendMessage(ChatColor.BLUE + "WEEKDAYS:" + ChatColor.AQUA + "3PM-9PM");
+    sender.sendMessage("message");
+    return true;
+  }
+}
--- io/github/rookietec9/Enderplugin/Enderplugin.java
+++ io/github/rookietec9/Enderplugin/Enderplugin.java
@@ -1,18 +1,36 @@
 package io.github.rookietec9.EnderPlugin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EnderPlugin extends JavaPlugin {
   public void onEnable() {
     getLogger().info("EnderPlugin by TEC9 has been successfully launched. :D");
+    getCommand("EnderSchedule").setExecutor(new EnderPluginCommandExecutor(this));
   }
   
   public void onDisable() {
     getLogger().info("EnderPlugin by TEC9 has shut down.");
   }
   
-  public boolean onCommand(CommandSender paramCommandSender, Command paramCommand, String paramString, String[] paramArrayOfString) {
-    throw new Error("Unresolved compilation problem: \n\tThis method must return a result of type boolean\n");
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    Player target = sender.getServer().getPlayer(args[0]);
+    if (cmd.getName().equalsIgnoreCase("enderHeal")) {
+      if (args.length < 1) {
+        sender.sendMessage(ChatColor.DARK_RED + "WHO?");
+        return false;
+      } 
+      if (target == null) {
+        sender.sendMessage(ChatColor.DARK_RED + args[0] + " is not currently cool.");
+        return true;
+      } 
+      target.setHealth(20.0D);
+      sender.sendMessage(ChatColor.AQUA + args[0] + " " + "was successfully healed. :D");
+      return true;
+    } 
+    return true;
   }
+}
