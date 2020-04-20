835bcd8f5c0d7953e6205ad1a8bf896931e88406
RookieTEC9 <jeremiur.ps101q@gmail.com>
2020-04-20 13:13:15 -0400

    Pre 0.1.1
    06/11/16
    
    7:46PM

--- io/github/rookietec9/Enderplugin/event/player/PlayerChat.java
+++ io/github/rookietec9/Enderplugin/event/player/PlayerChat.java
@@ -1,6 +1,6 @@
 package io.github.rookietec9.EnderPlugin.event.player;
 
-import net.md_5.bungee.api.ChatColor;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,7 +15,7 @@ public class PlayerChat implements Listener {
         Player sender = event.getPlayer();
         String Message = event.getMessage().toLowerCase();
         if (Message.contains("Rice")) {
-            sender.sendTitle(ChatColor.DARK_RED + "Tasty", ChatColor.RED + "YUMMY");
+            sender.sendMessage(ChatColor.DARK_RED + "Tasty");
         }
 
     }
