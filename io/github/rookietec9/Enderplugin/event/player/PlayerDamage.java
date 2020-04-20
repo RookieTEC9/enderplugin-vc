ad7040668a0355efceb956fb13734071773f44cf
RookieTEC9 <jeremiur.ps101q@gmail.com>
2020-04-20 13:32:38 -0400

    Pre 0.1.9
    06/13/16
    
    7:39PM

--- io/github/rookietec9/Enderplugin/event/player/PlayerDamage.java
+++ io/github/rookietec9/Enderplugin/event/player/PlayerDamage.java
@@ -1,5 +1,6 @@
 package io.github.rookietec9.EnderPlugin.event.player;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -15,11 +16,12 @@ public class PlayerDamage implements Listener {
     public void ondeath(EntityDamageByEntityEvent event) {
         if (event.getEntity().getType() == EntityType.PLAYER && event.getDamager().getType() == EntityType.ZOMBIE) {
             Player p = (Player)event.getEntity();
+            String DamagedName = String.valueOf(p.getName());
             String DamageTooken = String.valueOf(p.getLastDamage());
             String DamageMax = String.valueOf(p.getMaxHealth());
             if (p.getHealth() - event.getDamage() <= 0.0D) {
-                Bukkit.broadcastMessage(p + "Finishing Damage:" + DamageTooken);
-                Bukkit.broadcastMessage(DamageMax);
+                Bukkit.broadcastMessage(DamagedName + " Finishing Damage:" + DamageTooken);
+                Bukkit.broadcastMessage(ChatColor.RED + "Max health:" + ChatColor.RESET + DamageMax);
             }
         }
 
