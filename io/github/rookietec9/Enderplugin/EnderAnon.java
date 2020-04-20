c376407dfac5cf88b83c4e06d7f7a4b1df5b2951
RookieTEC9 <jeremiur.ps101q@gmail.com>
2020-04-20 15:05:34 -0400

    Pre 0.4.6
    06/17/16
    
    1:26PM

--- io/github/rookietec9/Enderplugin/EnderAnon.java
+++ io/github/rookietec9/Enderplugin/EnderAnon.java
@@ -1,5 +1,6 @@
 package io.github.rookietec9.EnderPlugin;
 
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -15,37 +16,32 @@ public class EnderAnon implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!command.getName().equalsIgnoreCase("enderAnon")) {
-            return true;
-        } else {
+        if (command.getName().equalsIgnoreCase("enderAnon")) {
             Player target = sender.getServer().getPlayer(args[0]);
             if (args.length < 2) {
                 sender.sendMessage(EnderPlugin.EnderPlugin + ChatColor.DARK_RED + "Command syntax: /endermessage <user> message");
                 return true;
-            } else if (target == null) {
-                sender.sendMessage(ChatColor.GOLD + "[ENDERPLUGIN PRE]" + ChatColor.DARK_RED + args[0] + " is not currently cool.");
-                return true;
-            } else {
-                if (args.length > 2) {
-                    for(int i = 0; i < args.length; ++i) {
-                        target.sendMessage(EnderPlugin.EnderPlugin + args[i]);
-                        sender.sendMessage("You sent the message to " + target.getName());
-                        if (target == sender) {
-                            Bukkit.broadcastMessage(EnderPlugin.EnderPlugin + ChatColor.DARK_BLUE + sender.getName() + " Is so sad they must message themselves.");
-                        }
+            }
 
-                        if (args.length == 2) {
-                            sender.sendMessage(EnderPlugin.EnderPlugin + args[1]);
-                            target.sendMessage(EnderPlugin.EnderPlugin + "You sent the message to " + sender.getName());
-                            if (target == sender) {
-                                Bukkit.broadcastMessage(EnderPlugin.EnderPlugin + ChatColor.DARK_BLUE + target.getName() + " Is so sad they must message themselves.");
-                            }
-                        }
-                    }
-                }
-
+            if (target == null) {
+                sender.sendMessage(ChatColor.GOLD + "[ENDERPLUGIN PRE] " + ChatColor.DARK_RED + args[0] + " is not currently cool.");
                 return true;
             }
+
+            if (args.length >= 2) {
+                String message = StringUtils.join(args, ' ', 1, args.length);
+                target.sendMessage(message);
+            }
+
+            if (args.length == 2) {
+                sender.sendMessage(EnderPlugin.EnderPlugin + args[1]);
+                target.sendMessage(EnderPlugin.EnderPlugin + "You sent the message to " + sender.getName());
+                if (target == sender) {
+                    Bukkit.broadcastMessage(EnderPlugin.EnderPlugin + ChatColor.DARK_BLUE + target.getName() + " Is so sad they must message themselves.");
+                }
+            }
         }
+
+        return true;
     }
 }
