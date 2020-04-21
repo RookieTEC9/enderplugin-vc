f5a2b3b170a463c24d3d652e4520f44560418038
RookieTEC9 <jeremiur.ps101q@gmail.com>
2020-04-20 20:28:29 -0400

    Pre 0.5.1
    06/17/16
    
    9:25PM

--- io/github/rookietec9/Enderplugin/EnderHeal.java
+++ io/github/rookietec9/Enderplugin/EnderHeal.java
@@ -7,6 +7,48 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+public class EnderHeal implements CommandExecutor {
+    private final EnderPlugin plugin;
+
+    public EnderHeal(EnderPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player target = sender.getServer().getPlayer(args[0]);
+        String SenderName = String.valueOf(sender.getName());
+        if (command.getName().equalsIgnoreCase("enderHeal")) {
+            if (args.length > 1) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("Plug Message")) + ChatColor.DARK_RED + "WHO?");
+                return true;
+            } else if (target == null) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("Plug Message")) + ChatColor.DARK_RED + args[0] + " is not currently cool.");
+                return true;
+            } else {
+                target.setHealth(20.0D);
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("Plug Message")) + ChatColor.AQUA + args[0] + " was successfully healed. :D");
+                Bukkit.broadcastMessage(this.plugin.getConfig().getString("Plug Message") + ChatColor.LIGHT_PURPLE + SenderName + " healed " + target.getName());
+                return true;
+            }
+        } else {
+            if (args.length == 0) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("Plug Message")) + ChatColor.AQUA + args[0] + " was successfully healed. :D");
+                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("Plug Message")) + ChatColor.LIGHT_PURPLE + SenderName + " healed " + sender.getName());
+            }
+
+            return true;
+        }
+    }
+}
+package io.github.rookietec9.EnderPlugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
 class EnderHeal implements CommandExecutor {
     private final EnderPlugin plugin;
 
