54704f526e7b71f1fd27b6cbf29359c39e4a4382
RookieTEC9 <jeremiur.ps101q@gmail.com>
2020-07-10 20:13:38 -0400

    Pre 0.7.4
    06/23/16
    
    1:20 PM

--- io/github/rookietec9/Enderplugin/EnderRank.java
+++ io/github/rookietec9/Enderplugin/EnderRank.java
@@ -14,50 +14,33 @@ public class EnderRank implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("EnderRank")) {
-            if (!(sender instanceof Player)) {
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("Plug Message")) + "This command can only be run by a player.");
+        if (!command.getName().equalsIgnoreCase("EnderRank")) {
+            return true;
+        } else if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("Plug Message")) + "This command can only be run by a player.");
+            return true;
+        } else {
+            Player playerSender = (Player)sender;
+            Player target = sender.getServer().getPlayer(args[0]);
+            String Name = String.valueOf(playerSender.getDisplayName());
+            String realName = String.valueOf(playerSender.getName());
+            String Owner = "§f[§EOWNER§f] ";
+            String RankCo = "§f[§cCO-OWNER§f] ";
+            String RankMember = "§f[§7SCRUB§F] ";
+            String ownerName = "";
+            ownerName = Owner.substring(0, ownerName.length());
+            String CoName = "";
+            CoName = RankCo.substring(0, ownerName.length());
+            String MemName = "";
+            MemName = RankMember.substring(0, ownerName.length());
+            if (args.length > 2) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("Plug Message")) + "Syntax: /enderRank User Level");
                 return true;
-            } else {
-                Player playerSender = (Player)sender;
-                Player target = sender.getServer().getPlayer(args[0]);
-                String Name = String.valueOf(playerSender.getDisplayName());
-                String realName = String.valueOf(playerSender.getName());
-                String Owner = "§f[§EOWNER§f] ";
-                String RankCo = "§f[§cCO-OWNER§f] ";
-                String RankMember = "§f[§7SCRUB§F] ";
-                String ownerName = "";
-                ownerName = Owner.substring(0, ownerName.length());
-                String CoName = "";
-                CoName = RankCo.substring(0, ownerName.length());
-                String MemName = "";
-                MemName = RankMember.substring(0, ownerName.length());
-                if (args.length > 2) {
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("Plug Message")) + "Syntax: /enderRank User Level");
-                    return true;
-                } else if (!playerSender.isOp() && target != playerSender) {
-                    playerSender.sendMessage("You cannot put a rank on this player.");
-                    return true;
-                } else if (args.length == 1) {
-                    if (args[0].toUpperCase() == "OWNER") {
-                        playerSender.setDisplayName(ownerName + Name);
-                        playerSender.setPlayerListName(ownerName + realName);
-                        return true;
-                    } else if (args[0].toUpperCase() == "CO") {
-                        playerSender.setDisplayName(CoName + Name);
-                        playerSender.setPlayerListName(CoName + realName);
-                        return true;
-                    } else if (args[0].toUpperCase() == "MEMBER") {
-                        playerSender.setDisplayName(MemName + Name);
-                        playerSender.setPlayerListName(MemName + realName);
-                        return true;
-                    } else if (args[0].toUpperCase() == "MEMBER" && args[0] != "CO" && args[0] != "OWNER") {
-                        return true;
-                    } else {
-                        sender.sendMessage("Please enter either " + ChatColor.YELLOW + "OWNER " + ChatColor.DARK_BLUE + "CO §7MEMBER");
-                        return true;
-                    }
-                } else if (target == null) {
+            } else if (!playerSender.isOp() && target != playerSender) {
+                playerSender.sendMessage("You cannot put a rank on this player.");
+                return true;
+            } else if (args.length != 1) {
+                if (target == null) {
                     sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("Plug Message")) + "Request Player is not online.");
                     return true;
                 } else if (args[1].toUpperCase() == "OWNER") {
@@ -76,9 +59,35 @@ public class EnderRank implements CommandExecutor {
                     sender.sendMessage("Please enter either " + ChatColor.YELLOW + "OWNER " + ChatColor.DARK_BLUE + "CO §7MEMBER");
                     return true;
                 }
+            } else {
+                if (args[0].toUpperCase() == "MEMBER" || args[0] == "CO" || args[0] == "OWNER") {
+                    if (args[0].toUpperCase() == "OWNER") {
+                        playerSender.setDisplayName(ownerName + Name);
+                        playerSender.setPlayerListName(ownerName + realName);
+                        return true;
+                    }
+
+                    if (args[0].toUpperCase() == "CO") {
+                        playerSender.setDisplayName(CoName + Name);
+                        playerSender.setPlayerListName(CoName + realName);
+                        return true;
+                    }
+
+                    if (args[0].toUpperCase() == "MEMBER") {
+                        playerSender.setDisplayName(MemName + Name);
+                        playerSender.setPlayerListName(MemName + realName);
+                        return true;
+                    }
+                }
+
+                if (args[0].toUpperCase() == "MEMBER" && args[0] != "CO" && args[0] != "OWNER") {
+                    return true;
+                } else {
+                    sender.sendMessage("Please enter either " + ChatColor.YELLOW + "OWNER " + ChatColor.DARK_BLUE + "CO §7MEMBER");
+                    sender.sendMessage(args[0]);
+                    return true;
+                }
             }
-        } else {
-            return true;
         }
     }
 }
